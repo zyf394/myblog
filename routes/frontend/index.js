63adfe7b1d7c96f539e3673c85453dc77bee3ad2
zhongyufei <zhongyufei@didichuxing.com.cn>
2016-07-13 01:28:37 +0800

    fix let

--- routes/frontend/index.js
+++ routes/frontend/index.js
@@ -11,7 +11,7 @@ var profile = require('./profile'),
 /* GET home page. */
 function frontendRoutes() {
     router.get('/', function(req, res, next) {
-        let indexPath = path.resolve(__dirname,'../../myblog/dist/index.html');
+        var indexPath = path.resolve(__dirname,'../../myblog/dist/index.html');
          fs.readFile(indexPath, function (err, data) {
              if(err) console.log(err)
              res.end(data)
